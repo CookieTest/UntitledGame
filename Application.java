f70d9f7893b28bd45fdf565dc7d27706026712ac
CookieTest <keksikoe1234@gmail.com>
2016-09-01 10:11:34 +0300

    Pohjasta tiedostoja
    Application ja Board listaan.

---
+++ Application.java
@@ -0,0 +1,42 @@
+//Do stuff (google java 2d game tutorial-> zetcode & gametutorial)
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.awt.event.KeyEvent;//Selvitä
+public class Application extends JFrame{
+	
+	public Application(){
+		
+		initUI();
+		
+	}
+	
+	private void initUI(){
+		
+		add(new Board());
+		
+		this.setUndecorated(false);
+		//this.setExtendedState(this.MAXIMIZED_BOTH);
+		
+		setTitle("Application");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+	}
+	
+	public static void main(String []args){
+		
+		EventQueue.invokeLater(new Runnable(){
+			
+			@Override
+			public void run(){
+				
+				Application ex = new Application();
+				ex.setVisible(true);
+				
+			}
+			
+		});
+		
+	}
+	
+}
---
+++ Board.java
@@ -0,0 +1,6 @@
+import javax.swing.JPanel;
+public class Board extends JPanel{
+	//Do stuff (google java 2d game tutorial)
+	public Board(){} 	
+	
+}
